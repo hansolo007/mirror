4bb05748db892b64416eabcb1fa019aea1713d92
hansolo007 <hanhuece@gmail.com>
2015-06-22 16:13:06 -0700

    stuff

--- homeserver/home/ws.js
+++ homeserver/home/ws.js
@@ -1,5 +1,3 @@
-
-
 function wsconnect(){
 	var ws = new WebSocket("ws://localhost:8888/ws");
         ws.onopen = function() {
@@ -7,24 +5,59 @@ function wsconnect(){
         };
 
         ws.onmessage = function (evt) {
-                console.log(evt.data);
-		if (evt.data.indexOf('}') != -1){
-			var sms = JSON.parse(evt.data.toString());
-			var box = document.getElementById('smsbox');
-			var table = document.createElement('table');
-			for (var i=0; i < sms.length; i++){
-				var tr = document.createElement("tr");
-				var name_th = document.createElement("th");
-				var text_th = document.createElement("th");
-				name_th.setAttribute('class','smsbox-container-name')
-				text_th.setAttribute('class','smsbox-container-text')
-				name_th.innerHTML = sms[i].name;
-				text_th.innerHTML = sms[i].text;
-				tr.appendChild(name_th);
-				tr.appendChild(text_th);
-				table.appendChild(tr);
-			}
-			box.appendChild(table);
+        	console.log(evt.data);
+			if (evt.data.indexOf('}') != -1){
+				var sms = JSON.parse(evt.data.toString());
+        		writeToContainer(sms);
+			};
 		}
-        };
+}
+
+
+function writeToContainer(sms_list){
+
+	var MAX_TABLE_ROW = 35;
+	var box = document.getElementById('smsbox');
+	var table = document.getElementById('sms-table');
+	if (!table){
+		table = document.createElement('table');
+		table.id = 'sms-table';
+	}
+    var size = table.children.length;
+    var list_size = sms_list.length;
+	var indexRemove = size + list_size - MAX_TABLE_ROW;
+	if (indexRemove > MAX_TABLE_ROW){
+		indexRemove = MAX_TABLE_ROW;
+	}
+
+	if (indexRemove > -1 && size != 0){
+		for (var i=0; i < indexRemove; i++){
+		  	table.children[0].remove();
+		}
+
+		for (var i=0; i < table.children.length; i++){
+			table.children[i].setAttribute('class','smsbox-container-oldText');
+		}
+
+	}
+	var ind = list_size - MAX_TABLE_ROW;
+	if (ind < 0){
+		ind = 0;
+	}
+
+	for (var i = ind ; i< list_size; i++){
+			var tr = document.createElement("tr");
+			var name_th = document.createElement("th");
+			var text_th = document.createElement("th");
+			name_th.setAttribute('class', 'smsbox-container-name');
+			tr.setAttribute('class', 'smsbox-container-newText');
+			name_th.innerHTML = sms_list[i].name;
+			text_th.innerHTML = sms_list[i].text;
+			tr.appendChild(name_th);
+			tr.appendChild(text_th);
+			table.appendChild(tr);
+	}
+	box.appendChild(table);
+
+
 }
